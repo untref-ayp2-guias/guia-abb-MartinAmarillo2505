e1693e6083daf8cf469c30b9074d198668a7b598
Martín Amarillo <46434353+MartinAmarillo2505@users.noreply.github.com>
2025-06-02 19:26:06 -0300

    Resolver ejercicio 1
    Co-authored-by: Ignacio Merino Mesa <imerinomesa@gmail.com>

--- ejercicios/ej01.go
+++ ejercicios/ej01.go
@@ -1,17 +1,36 @@
 package ejercicios
 
 import (
+	"errors"
 	"untref-ayp2/guia-abb/binarytree"
 )
 
 func SecondLargestElement(bt *binarytree.BinarySearchTree[int]) (int, error) {
-	// Implementar
-	var x int
-	return x, nil
+	if bt == nil {
+		return 0, errors.New("El árbol no puede ser nil")
+	}
+	return findSecLargeElem(bt.GetRoot())
 }
 
 func findSecLargeElem(n *binarytree.BinaryNode[int]) (int, error) {
-	// Implementar
-	var x int
-	return x, nil
+	if n == nil {
+		return 0, errors.New("No hay valores")
+	}
+
+	right := n.GetRight()
+	if right == nil {
+		if left := n.GetLeft(); left != nil {
+			return left.GetData(), nil
+		}
+		return 0, errors.New("No hay más de un elemento")
+	}
+
+	if right.GetRight() == nil {
+		if rLeft := right.GetLeft(); rLeft != nil {
+			return rLeft.GetData(), nil
+		}
+		return n.GetData(), nil
+	}
+
+	return findSecLargeElem(right)
 }
